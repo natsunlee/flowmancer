3f75f910deb040e4be5ef2ca5b17e7ca2aa6cdc3
Nathan Lee <lee.nathan.sh@gmail.com>
2022-01-05 00:17:17 -0500

    Update log naming pattern

--- flowmancer/logger/file.py
+++ flowmancer/logger/file.py
@@ -1,12 +1,13 @@
 import logging, os
-
+from datetime import datetime
 from ..jobspec.schema.v0_1 import FileLoggerDefinition
 from .logger import Logger
 
 class FileLogger(Logger):
     def __init__(self, task_name: str, detl: FileLoggerDefinition) -> None:
         os.makedirs(detl.path, exist_ok=True)
-        self.filename = f"{detl.path}/{task_name}.log"
+        ts_str = datetime.now().strftime("%Y-%m-%d.%H.%M.%S")
+        self.filename = f"{detl.path}/{ts_str}.{task_name}.log"
         self._level = logging.INFO
     
     def prepare(self) -> None:
