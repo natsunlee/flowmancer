efcde935892cd8c7f2f0d02214fb5bf25337d1d3
Nathan Lee <lee.nathan.sh@outlook.com>
2024-02-06 22:18:26 -0500

    Removal of TODO comments

--- flowmancer/jobdefinition/file.py
+++ flowmancer/jobdefinition/file.py
@@ -1,33 +1,26 @@
 import os
 import re
-from typing import Any, Callable, Pattern
+from typing import Any
 
 import yaml
 
 from . import JobDefinition, SerializableJobDefinition, job_definition
 
-_env_var_matcher = re.compile(r'\$ENV{([^}^{]+)}')
-_env_tag_matcher = re.compile(r'[^$]*\$ENV{([^}^{]+)}.*')
 
-# TODO: parameters file
-# _param_var_matcher = re.compile(r'\$PARAM{([^}^{]+)}')
-# _param_tag_matcher = re.compile(r'[^$]*\$PARAM{([^}^{]+)}.*')
-
-
-def _build_constructor(var_matcher: Pattern[str]) -> Callable[[Any, Any], str]:
-    def _path_constructor(_: Any, node: Any) -> str:
-        def replace_fn(match):
-            parts = f"{match.group(1)}:".split(":")
-            return os.environ.get(parts[0], parts[1])
-        return var_matcher.sub(replace_fn, node.value)
-    return _path_constructor
+def _path_constructor(_: Any, node: Any) -> str:
+    def replace_fn(match):
+        parts = f"{match.group(1)}:".split(":")
+        return os.environ.get(parts[0], parts[1])
+    _env_var_matcher = re.compile(r'\$ENV{([^}^{]+)}')
+    return _env_var_matcher.sub(replace_fn, node.value)
 
 
 @job_definition('yaml')
 class YAMLJobDefinition(SerializableJobDefinition):
     def load(self, filename: str) -> JobDefinition:
+        _env_tag_matcher = re.compile(r'[^$]*\$ENV{([^}^{]+)}.*')
         yaml.add_implicit_resolver("!envvar", _env_tag_matcher, None, yaml.SafeLoader)
-        yaml.add_constructor("!envvar", _build_constructor(_env_var_matcher), yaml.SafeLoader)
+        yaml.add_constructor("!envvar", _path_constructor, yaml.SafeLoader)
         with open(filename, 'r') as f:
             return JobDefinition(**yaml.safe_load(f.read()))
 
